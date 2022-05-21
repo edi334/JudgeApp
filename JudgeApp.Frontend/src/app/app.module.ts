f83fa95e7534bd05c8a40d825b2851db106b58db
Alexandru Lolea <alexandru.lolea@ligaac.ro>
2022-05-21 09:22:18 +0300

    Routing

--- JudgeApp.Frontend/src/app/app.module.ts
+++ JudgeApp.Frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
+import { JudgingPageComponent } from './components/judging-page/judging-page.component';
+import { ResultPageComponent } from './components/result-page/result-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    ProjectPageComponent,
+    JudgingPageComponent,
+    ResultPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ JudgeApp.Frontend/src/app/components/judging-page/judging-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-judging-page',
+  templateUrl: './judging-page.component.html',
+  styleUrls: ['./judging-page.component.scss']
+})
+export class JudgingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
